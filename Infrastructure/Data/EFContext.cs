0ca148818f63a3a4211284a7a91a31fc5891a351
Do Minh Hieu <hieudm@ichiba.vn>
2022-02-21 12:57:44 +0700

    update

---
+++ Infrastructure/Data/EFContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options) : base(options)
+        {
+        }
+    }
+}
